aec16ea37d30887f4248119e81bf89525cad53e8
dbcarelli <dbcarelli@gmail.com>
2018-03-30 14:04:31 +0400

    Clean up repo. delete old files.

--- mainComputerControl/speedChangeTest.m
+++
@@ -1,5 +0,0 @@
-for i = 100:-2.5:4
-    fwrite(xBee, char(round(i)), 'char')
-    disp(i)
-    pause(0.1)
-end
--- serialConnection.py
+++
@@ -1,5 +0,0 @@
-import serial
-ser = serial.Serial('/dev/tty.usbserial-A901QN3E')
-print(ser.name)         # check which port was really used
-ser.write(b'a')     # write a string
-ser.close()             # close port
