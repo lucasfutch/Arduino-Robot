ee8c3dbcdaf03cd45e79e72d60d609738d51143b
dbcarelli <dbcarelli@gmail.com>
2018-04-26 17:28:51 +0400

    Add environment class

---
+++ mainComputerControl/environment.py
@@ -0,0 +1,91 @@
+import time
+import random
+
+from rover import Rover
+from tracker import Tracker
+
+class Environment():
+    def __init__(self, system_time_step):
+        # system
+        self.system_tracker = Tracker()
+        self.timer = time.time()
+        self.system_time_step = system_time_step
+        self.pursuer_id = 0
+        self.evader_id = 2
+
+        # robots
+        self.pursuer = Rover(tracker=self.system_tracker,
+                             my_id=self.pursuer_id,
+                             target_id=self.evader_id,
+                             time_step=self.system_time_step,
+                             comm_port='COM5')
+
+        self.evader = Rover(tracker=self.system_tracker,
+                            my_id=self.evader_id,
+                            target_id=None,
+                            time_step=self.system_time_step,
+                            max_pivot_input=100,
+                            forward_speed=100,
+                            pivot_threshold=30,
+                            proportional_gain=5,
+                            integrator_gain=0,
+                            reversed=True,
+                            comm_port='COM9')
+
+    def step(self, target_heading=None, target_position=None):
+        # update environmate state
+        self.system_tracker.update()
+
+        if ((target_heading==None) and (target_position == None)):
+            return None
+
+        # update system state
+        elif (target_heading):
+            self.pursuer.update_state()
+            self.evader.update_state(desired_heading=target_heading)
+
+        # update system state
+        elif(target_position):
+            self.pursuer.update_state()
+            self.evader.update_state(target_pos=target_position)
+
+        # update actions
+        self.pursuer.update_action()
+        self.evader.update_action()
+
+        # wait for there to be information to report and update
+        time.sleep(self.system_time_step)
+        self.system_tracker.update()
+
+        # return the system state
+        evader_x = self.system_tracker.get_pos(self.evader_id)[0]
+        evader_y = self.system_tracker.get_pos(self.evader_id)[1]
+        evader_heading = self.system_tracker.get_heading(self.evader_id)
+        pursuer_x = self.system_tracker.get_pos(self.pursuer_id)[0]
+        pursuer_y = self.system_tracker.get_pos(self.pursuer_id)[1]
+        pursuer_heading = self.system_tracker.get_heading(self.pursuer_id)
+
+        return (evader_x, evader_y, evader_heading, pursuer_x, pursuer_y, pursuer_heading)
+
+    def reset(self):
+        # generate a random starting position
+        pursuer_x = random.randrange(20, 800, 1)/1000.0
+        pursuer_y = random.randrange(20, 800, 1)/1000.0
+        evader_x = random.randrange(20, 800, 1)/1000.0
+        evader_y = random.randrange(20, 800, 1)/1000.0
+
+        pursuer_target = [pursuer_x, pursuer_y]
+        evader_target = [evader_x, evader_y]
+
+        # tell robots to go to their reset locations
+        self.pursuer.update_state(target_pos=pursuer_target)
+        self.evader.update_state(target_pos=evader_target)
+
+        # wait until they have arrived
+        while ((not self.pursuer.navigator.has_arrived()) and
+               (not self.evader.navigator.has_arrived())):
+            # keep moving toward the targets
+            self.system_tracker.update()
+            self.pursuer.update_action()
+            self.evader.update_action()
+            time.sleep(0.001)
