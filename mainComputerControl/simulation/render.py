062448b267f7e93d0e0b61c4bec9edc8ec01b5e8
dbcarelli <dbcarelli@gmail.com>
2018-07-04 20:38:39 -0700

    first working dinamic

--- mainComputerControl/simulation/render.py
+++ mainComputerControl/simulation/render.py
@@ -1,34 +1,109 @@
 import pygame
 import numpy as np
+import time
+
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+darkBlue = (0,0,128)
+white = (255,255,255)
+black = (0,0,0)
+pink = (255,200,200)
 
 class RoverShape:
     def __init__(self):
-        self.shape = [np.array([0,1]),
-                      np.array([-1,-1]),
-                      np.array([1,-1])]
+        """
+        self.shape = [np.array([0,-15]),
+                      np.array([-10,10]),
+                      np.array([10,10]),
+                      np.array([0,-15])]
 
         self.points = [np.array([0,1]),
-                      np.array([-1,-1]),
-                      np.array([1,-1])]]
+                       np.array([-1,-1]),
+                       np.array([1,-1]),
+                       np.array([0,1])]
+        """
+        self.shape = [np.array([0,5]),
+                      np.array([0,-5])]
+        self.points = [np.array([0,5]),
+                       np.array([0,-5])]
 
-    def move(translation, rotation):
+    def move(self, translation, rotation):
 
         # rotate and translate shape
-        rad = -1*degree*np.pi/180 # rotate clockwise
-        rotation_matrix = np.array([np.cos(rad), -np.sin(rad)],
-                                   [np.sin(rad), np.cos(rad)])
+        rad = rotation*np.pi/180 # rotate clockwise
+        rotation_matrix = np.array([[np.cos(rad), -np.sin(rad)], \
+                                    [np.sin(rad), np.cos(rad)]])
 
         for i in range(len(self.points)):
-            self.point[i] = np.matmul(rotation_matrix, self.shape[i])
-            self.point[i][0] += translation[0]
-            self.point[i][1] += translation[1]
+            self.points[i] = np.matmul(rotation_matrix, self.shape[i])
+            self.points[i][0] += translation[0]
+            self.points[i][1] += translation[1]
 
-class RoverRender
-    def __init__(self, starting_position,starting_position):
+    def get_tuple_points(self):
+        tuple_list = []
+        for coordinate in self.points:
+            tuple_list.append(tuple(coordinate))
+        return tuple_list
+
+class RoverRender:
+    def __init__(self, starting_position, starting_heading):
         self.position = starting_position
         self.heading = starting_heading
         self.rover_shape = RoverShape()
-        screen = pygame.display.set_mode((640,480))
 
-    def render(position, heading):
+        # start screen
+        self.screen = pygame.display.set_mode((640,480))
+        self.screen.fill(white)
+        self.render(self.position, self.heading)
+
+    def render(self, position, heading):
         self.rover_shape.move(position, heading)
+        #self.screen.fill(white)
+        pygame.draw.lines(self.screen, black, False, self.rover_shape.get_tuple_points(), 1)
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    import curses
+
+    # get the curses screen window
+    screen = curses.initscr()
+
+    # turn off input echoing
+    curses.noecho()
+
+    # respond to keys immediately (don't wait for enter)
+    curses.cbreak()
+
+    # map arrow keys to special values
+    screen.keypad(True)
+
+    # stae variables
+    pos = [320, 240]
+    heading = 0
+    rover = RoverRender(pos, heading)
+
+    try:
+        while True:
+            char = screen.getch()
+            heading_rotated = heading + 90
+            if char == ord('q'):
+                break
+            elif char == curses.KEY_RIGHT:
+                # print doesn't work with curses, use addstr instead
+                heading += 5
+            elif char == curses.KEY_LEFT:
+                heading -= 5
+            elif char == curses.KEY_UP:
+                pos[0] -= np.arccos(heading_rotated)
+                pos[1] += np.arcsin(heading_rotated)
+            elif char == curses.KEY_DOWN:
+                pos[0] += np.arccos(heading_rotated)
+                pos[1] -= np.arcsin(heading_rotated)
+            print heading
+            rover.render(pos, heading)
+    finally:
+        # shut down cleanly
+        curses.nocbreak(); screen.keypad(0); curses.echo()
+        curses.endwin()
---
+++ mainComputerControl/simulation/rover_dynamics.py
@@ -0,0 +1,132 @@
+import numpy as np
+
+class RoverDynamics:
+    def __init__(self,
+                 wheel_diameter,
+                 wheel_separation,
+                 starting_position,
+                 starting_heading):
+
+        # robot parameters
+        self.wheel_diameter = wheel_diameter
+        self.wheel_separation = wheel_separation
+
+        # system state
+        self.position = starting_position
+        self.heading = starting_heading
+        self.turn_radius = None
+        self.d_theta = None
+
+    def update_state_pivot(self, left_motor_deg, right_motor_deg):
+        self.d_theta = self.get_linear_dist(left_motor_deg)/(np.pi*self.wheel_separation)*180
+        self.heading += self.d_theta
+
+    def update_state_forward_left(self, left_motor_deg, right_motor_deg):
+        s1 = self.get_linear_dist(right_motor_deg)
+        s2 = self.get_linear_dist(left_motor_deg)
+
+        self.d_theta = ((s1 - s2)/self.wheel_separation)*(180/np.pi)
+        self.heading -= self.d_theta
+        self.turn_radius = (s1*self.wheel_separation)/(s1-s2)
+
+        center_rad = self.turn_radius - (self.wheel_separation)/2.0
+        dx = -(center_rad - center_rad*np.cos(self.d_theta*np.pi/180))
+        dy = center_rad*np.sin(self.d_theta*np.pi/180)
+        displacement = np.array([[dx],[dy]])
+        print self.d_theta
+        print displacement
+
+        # rotate the displacement vector
+        rad = self.heading*np.pi/180.0
+        rotation_matrix = np.array([[np.cos(-rad), -np.sin(-rad)], [np.sin(-rad), np.cos(-rad)]])
+        displacement_rotated = np.matmul(rotation_matrix, displacement)
+
+        # calculate the final position of the the rover
+        print displacement_rotated
+        self.position[0] += displacement_rotated[0][0]
+        self.position[1] -= displacement_rotated[1][0]
+
+    def update_state_forward_right(self, left_motor_deg, right_motor_deg):
+        s1 = self.get_linear_dist(left_motor_deg)
+        s2 = self.get_linear_dist(right_motor_deg)
+
+        self.d_theta = ((s1 - s2)/self.wheel_separation)*(180/np.pi)
+        self.heading += self.d_theta
+        self.turn_radius = (s1*self.wheel_separation)/(s1-s2)
+
+        center_rad = self.turn_radius - (self.wheel_separation)/2.0
+        dx = -(center_rad - center_rad*np.cos(self.d_theta*np.pi/180))
+        dy = center_rad*np.sin(self.d_theta*np.pi/180)
+        displacement = np.array([[dx],[dy]])
+        print self.d_theta
+
+        # rotate the displacement vector
+        rad = self.heading*np.pi/180.0
+        rotation_matrix = np.array([[np.cos(-rad), -np.sin(-rad)], [np.sin(-rad), np.cos(-rad)]])
+        displacement_rotated = np.matmul(rotation_matrix, displacement)
+
+        # calculate the final position of the the rover
+        self.position[0] += displacement_rotated[0][0]
+        self.position[1] -= displacement_rotated[1][0]
+
+    def move_straight(self, left_motor_deg, right_motor_deg):
+        # calcualte distance forward
+        dy = self.get_linear_dist(left_motor_deg)
+        displacement = np.array([[0],[dy]])
+
+        # rotate the displacement vector
+        rad = self.heading*np.pi/180.0
+        rotation_matrix = np.array([[np.cos(-rad), -np.sin(-rad)], [np.sin(-rad), np.cos(-rad)]])
+        displacement_rotated = np.matmul(rotation_matrix, displacement)
+
+        # calculate the final position of the the rover
+        self.position[0] += displacement_rotated[0][0]
+        self.position[1] -= displacement_rotated[1][0]
+
+    def update_state(self, left_motor_deg, right_motor_deg):
+        # evaluate type of motion
+        if (left_motor_deg == -1*right_motor_deg):
+            self.update_state_pivot(left_motor_deg, right_motor_deg)
+            print "Pivoting"
+        elif (left_motor_deg < right_motor_deg):
+            self.update_state_forward_left(left_motor_deg, right_motor_deg)
+            print "Veering left"
+        elif (left_motor_deg > right_motor_deg):
+            self.update_state_forward_right(left_motor_deg, right_motor_deg)
+            print "Veering right"
+        else:
+            self.move_straight(left_motor_deg, right_motor_deg)
+            print "Mooving straight"
+
+    def get_linear_dist(self, angle_deg):
+        return self.wheel_diameter*angle_deg*np.pi/180.0
+
+    def get_new_heading(self):
+        return self.heading
+
+    def get_new_position(self):
+        return self.position
+
+if __name__ == '__main__':
+    from render import RoverRender
+    # state variables
+    pos = [320, 240]
+    heading = 0
+    rover_rendering = RoverRender(pos, heading)
+    rover = RoverDynamics(wheel_diameter=5,
+                          wheel_separation=20,
+                          starting_position=pos,
+                          starting_heading=heading)
+
+    while True:
+        a = raw_input("Enter action: ")
+        if a == "w":
+            rover.update_state(30, 30)
+        elif a =="a":
+            rover.update_state(10, 20)
+        elif a =="d":
+            rover.update_state(20, 10)
+        elif a=="s":
+            rover.update_state(-10, -10)
+
+        rover_rendering.render(rover.get_new_position(), rover.get_new_heading())
