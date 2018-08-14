5229992083a0d108fe0e0956232e1821f8915615
dbcarelli <dbcarelli@gmail.com>
2018-08-13 22:21:02 -0700

    crease separte file for coulomb agent simulation

--- mainComputerControl/environments/simulation_environment.py
+++ mainComputerControl/environments/simulation_environment.py
@@ -5,14 +5,18 @@ from agents.coulomb_agent import CoulombAgent
 
 
 class SimulationEnvironment:
-    def __init__(self, render=True):
+    def __init__(self,
+                 render=True,
+                 evader_start=[300, 500],
+                 pursuer_start=[700,500]):
+
         self.render = render
         self.arena_height = 1000
         self.arena_width = 1000
 
         self.evader = SimulatedRover(time_step=0.1,
                                      starting_heading=0,
-                                     starting_position=[300, 500],
+                                     starting_position=evader_start,
                                      max_pivot_input=50,
                                      forward_speed=100,
                                      pivot_threshold=30,
@@ -21,14 +25,12 @@ class SimulationEnvironment:
 
         self.pursuer = SimulatedRover(time_step=0.1,
                                       starting_heading=0,
-                                      starting_position=[700, 500],
+                                      starting_position=pursuer_start,
                                       max_pivot_input=50,
                                       forward_speed=70,
                                       pivot_threshold=30,
                                       proportional_gain=5,
                                       integrator_gain=0)
-
-        self.evader_agent = CoulombAgent(self)
 
         self.pursuer.target = self.evader
 
@@ -36,11 +38,15 @@ class SimulationEnvironment:
             from simulation.render import RoverRender
             self.rover_rendering = RoverRender(self.arena_height, self.arena_width)
 
-    def get_system_state(self):
-        pass
+    def system_state(self):
+        return (self.evader.pos,
+                self.evader.current_heading,
+                self.pursuer.pos,
+                self.pursuer.current_heading,
+                self.pursuer.navigator.has_arrived())
 
-    def step(self):
-        self.evader.update_state(desired_heading=self.evader_agent.get_target_heading())
+    def step(self, evader_desired_heading):
+        self.evader.update_state(desired_heading=evader_desired_heading)
         self.pursuer.update_state()
 
         self.evader.update_action()
@@ -56,13 +62,7 @@ class SimulationEnvironment:
                                       (255,0,0)) # red)
             self.rover_rendering.render()
 
-    def reset(self):
-        pass
-
-if __name__ == '__main__':
-
-    env = SimulationEnvironment()
-
-    for i in range(10000):
-        env.step()
-        time.sleep(0.02)
+    def reset(self, evader_starting_position, pursuer_starting_position):
+        self.__init__(render=self.render,
+                      evader_start=pursuer_starting_position,
+                      pursuer_start=evader_starting_position)
---
+++ mainComputerControl/runnables/coulomb_agent_simulation.py
@@ -0,0 +1,33 @@
+from environments.simulation_environment import SimulationEnvironment
+from agents.coulomb_agent import CoulombAgent
+import numpy as np
+
+ROUNDS = 10
+EPISODES = 500
+
+if __name__ == '__main__':
+
+    # Create the simulation environment
+    env = SimulationEnvironment(render=True)
+
+    # Create the evading agent
+    evader_agent = CoulombAgent(env)
+
+    for round in range(ROUNDS):
+        # generate starting positions
+        evader_start = list(np.random.random_integers(1000, size=2))
+        pursuer_start = list(np.random.random_integers(1000, size=2))
+
+        # reset environment
+        env.reset(evader_start, pursuer_start)
+
+        # Play the game
+        for episode in range(EPISODES):
+            evader_target = evader_agent.get_target_heading()
+            env.step(evader_desired_heading=evader_target)
+
+            state = env.system_state()
+
+            if state[4] == True:
+                print "GAME OVER"
+                break
